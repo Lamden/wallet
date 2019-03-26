50d0ec2de04a351fe96cd3731db00e82aacde04a
JeffWScott <jeffscott@outlook.com>
2019-03-25 22:36:45 -0400

    typo

--- src/popup/components/firstRun.vue
+++ src/popup/components/firstRun.vue
@@ -99,7 +99,7 @@ export default {
         content: [
           "Welcome to the Lamden Wallet. This is a place to store your Lamden TAU tokens but also an interface with our Clove Atomic Swap website",
           "To interface with the Clove website we will need to store your private keys in this wallet. We use your keys to sign transacations during the Atomic Swap process.",
-          "We will encrypt your keys in the broswer usng a secure password you will create. Failure to keep this password safe could result in your funds being unrecoverable or stolen."
+          "We will encrypt your keys in the broswer using a secure password you will create. Failure to keep this password safe could result in your funds being unrecoverable or stolen."
         ]
     };
   },
