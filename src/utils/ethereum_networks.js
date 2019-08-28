a11c1745dc0a493a82fcc24e00037d925593c273
JeffWScott <jeffscott@outlook.com>
2019-08-28 16:17:16 -0400

    removing old wallet code

--- src/utils/ethereum_networks.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  ETH: {tokenKey: 'EthereumETH'},
-  TAU: {tokenKey: 'LamdenTAU'},
-  ETC: {tokenKey: "Ethereum ClassicETC"},
-  VET: {tokenKey: "VeChainVET"},
-  REP: {tokenKey: "AugurREP"}
-};
