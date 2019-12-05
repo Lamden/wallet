941f6faa7b42a3626a1795fd8fbf91f725588657
JeffWScott <jeffscott@outlook.com>
2019-12-05 14:07:24 -0500

    All current tests passing when run individually.

--- cypress/integration/unit-tests/firstrun-restore-spec.js
+++ cypress/integration/unit-tests/firstrun-restore-spec.js
@@ -5,8 +5,6 @@ import 'cypress-file-upload';
 const fileName = 'files/Testing.keystore'
 
 describe('First Run Restore Wallet Process', () => {
-
-
     it('Loads FirstRunInto to start', () => {
         cy.fixture(fileName, 'utf-8').then(fileContent => {
             console.log(fileContent)
@@ -30,13 +28,11 @@ describe('First Run Restore Wallet Process', () => {
             })
             cy.get('#create-wallet').should('exist')      
             cy.get('#restore-wallet').should('exist')
-            cy.wait(500)
 
             //
             cy.log('Renders FirstTimeCreatePW when Restore Wallets button is selected')
             cy.get('#restore-wallet').focus().click();
             cy.get('.firstrun-create-pwd').should('exist')
-            cy.wait(500)
 
             //
             cy.log('Renders RestoreUpload.svelte after strong passwords are entered and button clicked')
@@ -44,13 +40,11 @@ describe('First Run Restore Wallet Process', () => {
             cy.get('input#pwd2').focus().invoke('attr', 'value', 'Testing0!0101')
             cy.get('[type="submit"]').focus().click()
             cy.get('.restore-upload').should('exist')
-            cy.wait(500)
 
             //
             cy.log('Button is enabled when file is uploaded to the picker')
             cy.get('#filePicker').should('exist').upload({ fileContent, fileName, mimeType: 'text/plain', encoding: 'utf-8' }, { subjectType: 'input', force: true });
             cy.get('#confirm-keystore-btn').should('exist').should('not.be.disabled').click();
-            cy.wait(1000)
 
             //
             cy.log('Renders RestorePassword when Password Hint is decoded and displayed')
@@ -58,14 +52,12 @@ describe('First Run Restore Wallet Process', () => {
             cy.get('#pwd-hint').then(($div) => {
                 expect($div[0].textContent).to.eq("This is for testing")
             })
-            cy.wait(500)
 
             //---
             cy.log('Renders RestoreAddWallets after successful password')
             cy.get('#pwd-input').focus().invoke('attr', 'value', 'Testing0!0101')
             cy.get('[type="submit"]').focus().click()
             cy.get('.restore-addwallets').should('exist')
-            cy.wait(500)
 
             //---
             cy.log('All checkboxes are selected when all wallets is checked')
@@ -73,14 +65,12 @@ describe('First Run Restore Wallet Process', () => {
             cy.get('#chkbox-0').then(($chkbox) => {
                 expect($chkbox[0].checked).to.be.true
             })
-            cy.wait(500)
 
             //---
             cy.log('Wallet VK is displayed')
             cy.get('#div-address-0').then(($div) => {
                 expect($div[0].textContent).to.eq("60d5ebffec35794e016643c31686d7354678a46493ab3432369fa820a8c36e49")
             })
-            cy.wait(500)
 
             //
             cy.log('Renders RestoreSaveWallets.svelte to save wallets to localstorage after Restore button pressed')
@@ -97,7 +87,7 @@ describe('First Run Restore Wallet Process', () => {
             cy.get('.firstrun-finishing').should('exist')
 
             //
-            cy.wait(5500)
+            cy.wait(5000)
             cy.get('.coinsmain').should('exist')
 
         })
