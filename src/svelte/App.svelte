b764b9f65e77fa5dda07f84a5bd63d30bf0c7892
JeffWScott <jeffscott@outlook.com>
2019-12-17 11:08:48 -0500

    remove this

--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -29,7 +29,7 @@
 
 	onMount(() => {
 		//prevent lockscreen from poping up durring testing//
-		CoinStore.setPwd('Testing0!0101')
+		//CoinStore.setPwd('Testing0!0101')
 		///
 		calcRemainingStorage();
 		document.querySelector("html").style = themes[$themeStyle];
