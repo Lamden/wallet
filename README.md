86678ae313de8c75ad8a7f36cfec79dc8d2916e3
Jeff Scott <jeff@Johns-MacBook-Pro.local>
2020-04-09 21:41:29 -0400

    version up

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Lamden Wallet
-version: 0.9.23 rc1
+version: 0.9.24
 
 ## Install
 
--- src/js/stores/settingsStore.js
+++ src/js/stores/settingsStore.js
@@ -10,7 +10,7 @@ import { isSettingsStoreObj,  isPageInfoObj} from '../objectValidations';
 const defualtSettingsStore = {
     'currentPage' : {'name': 'FirstRunMain', 'data' : {}},
     'themeStyle':'dark',
-    'version':'0.9.23'
+    'version':'0.9.24'
 }
 
 const createSettingsStore = () => {
