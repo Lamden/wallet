69a6eb8c1042d494cb0735eed9e1c2866cd82a43
jeffwscott <jeffscott@outlook.com>
2020-06-08 18:14:39 -0400

    converted IDE to use lintserver instead of mockchain

--- src/svelte/IDE/IdeMain.svelte
+++ src/svelte/IDE/IdeMain.svelte
@@ -21,6 +21,7 @@
 	let editorIsLoaded = false;  
 	let monaco;
 	let monacoComponent;
+	let CONTRACTING_API = "http://167.99.173.97/contracting"
 
 	onMount(() =>{
 		breadcrumbs.set([{name: 'Smart Contracts', page: {name: ''}}]);
@@ -42,15 +43,29 @@
 	}
 
 	const lint = async (callback) => {
-		let mockchain = $currentNetwork
-		if ($currentNetwork.type !== 'mockchain') mockchain = NetworksStore.getPublicMockchain()
-		if ($activeTab.code === '') lintErrors = {violations: null}
-		else {
-			lintErrors = await mockchain.API.lintCode($activeTab.name, $activeTab.code)
+		const sendCallback = (data) => {
+			if (Object.prototype.toString.call(callback) === '[object Function]') callback(data)
 		}
-		try {
-			callback(lintErrors);
-		} catch (e){}
+		if ($activeTab.code === '') {
+			sendCallback({violations: null})
+			lintErrors = {violations: null}
+		}else{
+			let response = fetch(`${CONTRACTING_API}/lint`,{
+				method: 'POST',
+				headers: {'Content-Type': 'application/json'},
+				body: JSON.stringify({
+					name: $activeTab.name, 
+					code: $activeTab.code
+				})
+			})
+			.catch(err => console.log(err))
+			.then(res => res.json())
+			.then(json =>{
+				console.log(json)
+				sendCallback(json)
+				lintErrors = json
+			})
+		}
 	}
 
 	const submit = (res) => {
