2ba0758bce5b6c85d54dcb70f5eb4a4116438435
jeffwscott <jeffscott@outlook.com>
2020-12-18 11:31:00 -0500

    remove console statement

--- src/svelte/IDE/IdeGetVariable.svelte
+++ src/svelte/IDE/IdeGetVariable.svelte
@@ -15,7 +15,7 @@
     const handleRun = async () => {
         try{
             $currentNetwork.API.getVariable(contractName, variableName, key)
-            .then(res => {console.log(res); result = res})
+            .then(res => {result = res})
             .catch(err => result = 'Error: Not Found')
         } catch (e) {
             result = 'Error: Not Found'
