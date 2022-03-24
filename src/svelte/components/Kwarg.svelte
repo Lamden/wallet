ff4a5681104c86637e221f3d82fbe91f50579ca4
dapiguabc <dapiguabc@163.com>
2022-03-24 10:10:02 +0800

    Allow setting type for "Any"

--- src/svelte/components/Kwarg.svelte
+++ src/svelte/components/Kwarg.svelte
@@ -1,72 +1,97 @@
 <script>
-    import { afterUpdate } from 'svelte'
-    import { createEventDispatcher } from 'svelte';
-    const dispatch = createEventDispatcher();
+  import { afterUpdate } from "svelte";
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
 
-	//Components
-    import { Components }  from '../Router.svelte';
-    const { Button, InputBox, DropDown, DatePicker, TimeDelta} = Components;
+  //Components
+  import KwargForAny from "./KwargForAny.svelte";
+  import { Components } from "../Router.svelte";
+  const { Button, InputBox, DropDown, DatePicker, TimeDelta } = Components;
 
-    //Utils
-    import { longFormTypes, typeToInputTypeMAP, defaultTypeValues } from '../../js/utils.js'    
+  //Utils
+  import {
+    longFormTypes,
+    typeToInputTypeMAP,
+    defaultTypeValues,
+  } from "../../js/utils.js";
 
-    export let argumentInfo;
-    export let index;
-    export let bgStyle = "primary";
+  export let argumentInfo;
+  export let index;
+  export let bgStyle = "primary";
 
-    let inputElm;
+  let inputElm;
 
-    let InputBoxTypes = ['Any', 'str', 'float', 'int', 'dict', 'list']
-    let DropDownTypes = ['bool']
-    let DatePickerTypes = ['datetime.datetime', 'datetime']
-    let TimeDeltaTypes = ['datetime.timedelta', 'timedelta']
+  let InputBoxTypes = ["str", "float", "int", "dict", "list"];
+  let DropDownTypes = ["bool"];
+  let DatePickerTypes = ["datetime.datetime", "datetime"];
+  let TimeDeltaTypes = ["datetime.timedelta", "timedelta"];
 
-    const handleDropDownSelection = (e) => {
-        argumentInfo.value = e.detail.selected.value
-        dispatch('argChanged')
-    }
-
+  const handleDropDownSelection = (e) => {
+    argumentInfo.value = e.detail.selected.value;
+    dispatch("argChanged");
+  };
 </script>
 
+{#if argumentInfo.type === "Any"}
+  <KwargForAny
+    {argumentInfo}
+    {index}
+    {bgStyle}
+    on:argChanged={() => dispatch("argChanged")}
+  />
+{/if}
+
 {#if InputBoxTypes.includes(argumentInfo.type)}
-    <InputBox
-        id={`kwarg-${index}`}
-        {bgStyle}
-        bind:value={argumentInfo.value}
-        bind:thisInput={inputElm}
-        placeholder={defaultTypeValues[argumentInfo.type]}
-        margin="0 0 1rem 0"
-        label={`${argumentInfo.name.toUpperCase()} (${longFormTypes[argumentInfo.type]})`}
-        inputType={typeToInputTypeMAP[argumentInfo.type]}
-        on:changed={() =>  dispatch('argChanged')} /> 
+  <InputBox
+    id={`kwarg-${index}`}
+    {bgStyle}
+    bind:value={argumentInfo.value}
+    bind:thisInput={inputElm}
+    placeholder={defaultTypeValues[argumentInfo.type]}
+    margin="0 0 1rem 0"
+    label={`${argumentInfo.name.toUpperCase()} (${
+      longFormTypes[argumentInfo.type]
+    })`}
+    inputType={typeToInputTypeMAP[argumentInfo.type]}
+    on:changed={() => dispatch("argChanged")}
+  />
 {/if}
 
 {#if DropDownTypes.includes(argumentInfo.type)}
-    <DropDown
-        {bgStyle}
-        items={defaultTypeValues[argumentInfo.type]} 
-        id={`kwarg-${index}`}
-        margin="0 0 1rem 0"
-        label={`${argumentInfo.name.toUpperCase()} (${longFormTypes[argumentInfo.type]})`} 
-        on:selected={handleDropDownSelection} />
+  <DropDown
+    {bgStyle}
+    items={defaultTypeValues[argumentInfo.type]}
+    id={`kwarg-${index}`}
+    margin="0 0 1rem 0"
+    label={`${argumentInfo.name.toUpperCase()} (${
+      longFormTypes[argumentInfo.type]
+    })`}
+    on:selected={handleDropDownSelection}
+  />
 {/if}
 
 {#if DatePickerTypes.includes(argumentInfo.type)}
-    <DatePicker 
-        {bgStyle}
-        id={`kwarg-${index}`}
-        bind:dateISO={argumentInfo.value}
-        margin="0 0 1rem 0"
-        label={`${argumentInfo.name.toUpperCase()} (${longFormTypes[argumentInfo.type]})`} 
-        on:changed={() =>  dispatch('argChanged')}/>
+  <DatePicker
+    {bgStyle}
+    id={`kwarg-${index}`}
+    bind:dateISO={argumentInfo.value}
+    margin="0 0 1rem 0"
+    label={`${argumentInfo.name.toUpperCase()} (${
+      longFormTypes[argumentInfo.type]
+    })`}
+    on:changed={() => dispatch("argChanged")}
+  />
 {/if}
 
 {#if TimeDeltaTypes.includes(argumentInfo.type)}
-    <TimeDelta 
-        {bgStyle}
-        id={`kwarg-${index}`}
-        bind:milliseconds={argumentInfo.value}
-        margin="0 0 1rem 0"
-        label={`${argumentInfo.name.toUpperCase()} (${longFormTypes[argumentInfo.type]})`} 
-        on:changed={() =>  dispatch('argChanged')}/>
+  <TimeDelta
+    {bgStyle}
+    id={`kwarg-${index}`}
+    bind:milliseconds={argumentInfo.value}
+    margin="0 0 1rem 0"
+    label={`${argumentInfo.name.toUpperCase()} (${
+      longFormTypes[argumentInfo.type]
+    })`}
+    on:changed={() => dispatch("argChanged")}
+  />
 {/if}
